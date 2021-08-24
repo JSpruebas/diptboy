aa5dc3f33eb51935bda5b5e28cb81313b03954d8
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-24 19:31:33 -0300

    Update coso.js

--- coso.js
+++ coso.js
@@ -3,12 +3,6 @@ const farmAdd = "0xe0979c566153602B24f7f07999cbFbc7D499eE66";
 const tokenAdd = "0xDaac95fa5761b808794e5D5b2C402350940c91e8";
 let chainId;
 let tokenContract, farmContract;
-let tuCuenta, tuBalance, allowance
-
-const aprobar1 = document.getElementById("botonAp1");
-const harvest1 = document.getElementById("botHarvest1");
-const mint1 = document.getElementById("button1");
-
 
 window.onload = async () => {
   const provider = await detectEthereumProvider({ timeout: 2000 })
@@ -39,25 +33,33 @@ window.onload = async () => {
 
 const coso = async () => {
 
-  tuCuenta = await web3.eth.getAccounts();
+  let tuCuenta = await web3.eth.getAccounts();
   document.getElementById("add").textContent = `${String(tuCuenta).substring(1, 5)}...${String(tuCuenta).substring(38)} `;
-  tuBalance = await web3.eth.getBalance(tuCuenta[0]);
+  let tuBalance = await web3.eth.getBalance(tuCuenta[0]);
   tuBalance = Number(web3.utils.fromWei(tuBalance)).toFixed(3);
   document.getElementById("bal").textContent = tuBalance;
 
   tokenContract = await new web3.eth.Contract(window.tokenAbi, tokenAdd);
   farmContract = await new web3.eth.Contract(window.farmAbi, farmAdd);
 
-  allowance = await tokenContract.methods.allowance(tuCuenta[0], farmAdd).call()
+  const aprobar1 = document.getElementById("botonAp1");
+  const harvest1 = document.getElementById("botHarvest1");
+  const mint1 = document.getElementById("button1");
 
-  if (allowance > 1e24) {
-    aprobar1.textContent = "DEPOSITAR"
-    aprobar1.addEventListener("click", funcDepo1);
-    aprobar1.removeEventListener("click", funcAprob1);
-  } else {
-    aprobar1.addEventListener("click", funcAprob1);
-    aprobar1.removeEventListener("click", funcDepo1);
-  } 
+  const chekAllowance = async () => {
+    let allowance = await tokenContract.methods.allowance(tuCuenta[0], farmAdd).call()
+    if (allowance > 1e24) {
+      aprobar1.textContent = "DEPOSITAR"
+      aprobar1.addEventListener("click", funcDepo1);
+    } else {
+      aprobar1.addEventListener("click", funcAprob1);
+    }
+  }
+
+  chekAllowance();
+
+  mint1.addEventListener("click", mint);
+  harvest1.addEventListener("click", funcHarvest1);
 
 
   let pendingReward = await farmContract.methods.pendingD20(0, tuCuenta[0]).call();
@@ -73,7 +75,7 @@ const coso = async () => {
 
 }
 
-function refrescar() { setInterval(coso, 5000) };
+function refrescar() { setInterval(coso, 30000) };
 
 
 const funcAprob1 = async () => {
@@ -97,10 +99,6 @@ async function mint() {
   await tokenContract.methods.mint(cuenta[0], BigInt(1e18)).send({ from: cuenta[0] });
 
 }
-
-
-mint1.addEventListener("click", mint);
-  harvest1.addEventListener("click", funcHarvest1);
 
 
 
